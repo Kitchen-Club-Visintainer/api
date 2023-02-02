0a78c16c0b3e35b68be40768a2410379215c00e2
davi <eusoudavi@gmail.com>
2023-02-01 23:44:22 -0300

    gerar token de autenticação

---
+++ src/main/java/br/com/kitchen/club/config/security/TokenService.java
@@ -0,0 +1,37 @@
+package br.com.kitchen.club.config.security;
+
+import br.com.kitchen.club.entity.Usuario;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
+
+import javax.validation.Valid;
+import java.util.Date;
+
+@Service
+public class TokenService {
+
+    @Value("${token.jwt.expiration}")
+    private String expiration;
+
+    @Value("${token.jwt.secret}")
+    private String secret;
+
+    public String gerarToken(Authentication authentication) {
+
+        Usuario logado = (Usuario) authentication.getPrincipal();
+        Date hoje = new Date();
+        Date dataExpiracao = new Date(hoje.getTime() + Long.parseLong(expiration));
+
+        return Jwts.builder()
+                .setIssuer("API Kitchen Club")
+                .setSubject(logado.getId().toString())
+                .setIssuedAt(hoje)
+                .setExpiration(dataExpiracao)
+                .signWith(SignatureAlgorithm.HS256, secret)
+                .compact();
+    }
+
+}
