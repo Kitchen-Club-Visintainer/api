9170b2633c2e43fc4cf9807aed5c4530f60b2462
davi <eusoudavi@gmail.com>
2023-02-01 23:50:57 -0300

    Post solicitando token

---
+++ src/main/java/br/com/kitchen/club/controller/AuthController.java
@@ -0,0 +1,42 @@
+package br.com.kitchen.club.controller;
+
+import br.com.kitchen.club.config.security.TokenService;
+import br.com.kitchen.club.controller.form.LoginForm;
+import br.com.kitchen.club.controller.tdo.TokenTdo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+    @Autowired
+    private AuthenticationManager authManager;
+
+    @Autowired
+    private TokenService tokenService;
+
+    @PostMapping
+    public ResponseEntity<TokenTdo> autenticar(@RequestBody @Valid LoginForm form) {
+        UsernamePasswordAuthenticationToken dadosLogin = form.converter();
+
+        try {
+            var authentication = authManager.authenticate(dadosLogin);
+            String token = tokenService.gerarToken(authentication);
+//            ELE ENVIA UM FORM COM AS INFORMAÇÕES DO TOKEN E O TIPO DE AUTENTICAÇÃO QUE A API DEVERÁ FAZER FUTURAMENTE
+            return ResponseEntity.ok(new TokenTdo(token, "Bearer"));
+        } catch (AuthenticationException e) {
+            return ResponseEntity.badRequest().build();
+        }
+    }
+
+}
