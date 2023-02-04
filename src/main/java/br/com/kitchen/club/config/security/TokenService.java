05951d72f58a084964bb9ea1f635188ee63f504b
davi <eusoudavi@gmail.com>
2023-02-03 21:53:11 -0300

    estrutura completa da TokenService

--- src/main/java/br/com/kitchen/club/config/security/TokenService.java
+++ src/main/java/br/com/kitchen/club/config/security/TokenService.java
@@ -1,17 +1,21 @@
 package br.com.kitchen.club.config.security;
 
 import br.com.kitchen.club.entity.Usuario;
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
-import javax.validation.Valid;
 import java.util.Date;
 
 @Service
 public class TokenService {
+
+    protected final Log logger = LogFactory.getLog(this.getClass());
 
     @Value("${token.jwt.expiration}")
     private String expiration;
@@ -34,4 +38,19 @@ public class TokenService {
                 .compact();
     }
 
+    public Boolean isTokenValido(String token) {
+        try {
+            Jwts.parser().setSigningKey(this.secret).parseClaimsJws(token);
+            return true;
+        } catch (Exception e) {
+            logger.error("Falha na autenticação" + e);
+            return false;
+        }
+    }
+
+    public Long getIdUsuario(String token) {
+        Claims body = Jwts.parser().setSigningKey(this.secret).parseClaimsJws(token).getBody();
+        String id = body.getSubject();
+        return Long.parseLong(id);
+    }
 }
